7d75b8abcc1a8f7ee21082d1a9134cd07e57d6a5
Panni <szabopanni@sztaki.hu>
2025-07-14 11:47:14 +0200

    Remove unused files and update .gitignore to include .DS_Store

--- build/lib/sticker/version.py
+++
@@ -1,6 +0,0 @@
-# Generated in setup.py
-
-git_tag = None
-git_revision = '4b96d236'
-version = '0.1.0+dev.4b96d236'
-linkified_version = '0.1.0+dev.[4b96d236](https://github.com/maunium/stickerpicker/commit/4b96d236212b1212976f4c3c60479e7aaed866cb)'
